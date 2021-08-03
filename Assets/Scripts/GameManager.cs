047461da7a7d0101c243fd921b6ce9d3a0d490ec
Miiya <40447362+musicangora@users.noreply.github.com>
2021-08-04 02:54:08 +0900

    Chapter6
    UIの追加と自動リスタート機能の追加
    実行すると激重になる症状が出ている

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,16 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public Text CountText;
     public GameObject ClearText;
     public int appleCount = 5;
+    private string _sceneName;
     // Start is called before the first frame update
     void Start()
     {
         ClearText.SetActive(false);
+        _sceneName = SceneManager.GetActiveScene().name;
     }
 
     // Update is called once per frame
@@ -21,11 +24,18 @@ public class GameManager : MonoBehaviour
         if (appleCount <= 0)
         {
             ClearText.SetActive(true);
+            StartCoroutine(Restart());  // コルーチンで2秒待ってからリスタート
         }
     }
 
     public void SubScore(int val)
     {
         appleCount -= val;
+    }
+
+    IEnumerator Restart()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(_sceneName);
     }
 }
