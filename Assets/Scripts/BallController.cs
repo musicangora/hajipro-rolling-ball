2043d08ad135f83a8c3f19fce4d8ef1068f8689c
Miiya <40447362+musicangora@users.noreply.github.com>
2021-08-04 00:17:52 +0900

    Chapter2
    ボールの移動とりんごを配置

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // 移動量調整用パラメータ
+    public float force = 1.0f;
+    private Rigidbody _rb;
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // 動かす力の初期化
+        Vector3 _forceVector = new Vector3();
+
+        if (Application.isMobilePlatform)
+        {
+            _forceVector.x = Input.acceleration.x;
+            _forceVector.z = Input.acceleration.z;
+        } else {
+            // キー入力を検知
+        _forceVector.x = Input.GetAxis("Horizontal");
+        _forceVector.z = Input.GetAxis("Vertical");
+        }
+
+        // 移動処理
+        _rb.AddForce(force * _forceVector);
     }
 }
